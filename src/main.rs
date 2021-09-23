a3e4cb76754f1f751609097fb25f4006d5d948e3
Werner Kroneman <w.kroneman@ucr.nl>
2021-09-23 18:04:36 +0200

    Multiple ships can now coexist.

--- src/main.rs
+++ src/main.rs
@@ -21,8 +21,6 @@ use typemap::TypeMap;
 
 use actors::System;
 use std::cell::RefCell;
-use blue_engine::header::{Engine, WindowDescriptor};
-use blue_engine::objects::triangle;
 use crate::actors::Fate::{Keep, End};
 use kiss3d::window::Window;
 use std::path::Path;
@@ -51,11 +49,31 @@ struct ScanPulse(Point3<f64>);
 const SCAN_PING_SPEED : f64 = 10.0;
 const SCAN_PING_RANGE : f64 = 1000.0;
 
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
+struct ShipId(u64);
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+struct ShipDestination(ShipId,Point3<f64>);
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
+struct ShipArrived(ShipId);
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+struct ShipMoved(ShipId,Point3<f64>);
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+struct CollectAsteroid(Point3<f64>);
+
+#[derive(Clone, Copy, Eq, PartialEq, Hash, Debug)]
+struct Score(u64);
+
 fn main() {
+
+    let mut system = System::new();
 
     let mut rng = thread_rng();
 
-    struct ShipMoved(Point3<f64>);
+    let mut window = Window::new("Kiss3d: cube");
 
     let asteroids = (0..10).map(|_| {
         Point3::new(
@@ -65,117 +83,131 @@ fn main() {
         )
     }).collect::<Vec<_>>();
 
-    let mut system = System::new();
-
-    struct ShipMovementController {
-        position: Point3<f64>,
-        destination: Option<Point3<f64>>
-    }
-
-    #[derive(Debug)]
-    struct ShipDestination(Point3<f64>);
-    struct ShipArrived;
-
-    #[derive(Debug)]
-    struct CollectAsteroid(Point3<f64>);
-
-    struct Score(u64);
-
-    system.build_actor(0)
-        .with_handler(move |score, _:&CollectAsteroid, outbox| {*score += 1; outbox.send(Score(*score)); Keep} );
-
-    system.build_actor(ShipMovementController {
-        position: Point3::origin(),
-        destination: None
-    }).with_handler(move |state, _:&Tick, outbox| {
-        if let Some(destination) = &state.destination {
-            const SPEED : f64 = 0.1;
-            if (destination - state.position).norm() < SPEED {
-                state.position = *destination;
-                state.destination = None;
-
-                outbox.send(ShipArrived);
-            } else {
-                state.position += (destination - state.position).normalize() * SPEED;
-            }
-        }
-        outbox.send(ShipMoved(state.position));
-
-        Keep
-    }).with_handler(move |state, ShipDestination(pos), outbox| {
-        eprintln!("Will go to destination: {:?}", pos.coords.data.as_slice());
-        state.destination = Some(*pos);
-
-        Keep
-    });
-
-    #[derive(PartialEq)]
-    enum ShipBehavior {
-        Ready,
-        WaitingForPing,
-        ApproachingAsteroid(Point3<f64>)
-    }
-
-    struct ShipBehaviorControllerState {
-        position: Point3<f64>, // This shouldn't be in here.
-        behavior: ShipBehavior
-    }
-
     #[derive(Clone)]
-    struct StartShip;
+    struct StartShip(ShipId);
 
-    system.build_actor(ShipBehaviorControllerState {
-        behavior: ShipBehavior::Ready,
-        position: Point3::origin()
-    })
-        .with_handler(move |state, _:&StartShip, outbox| {
-            outbox.send(ScanPulse(state.position));
-            state.behavior = ShipBehavior::WaitingForPing;
+    for ship_id in 0..5 {
 
-            Keep
-        })
-        .with_handler(move |state, ShipMoved(to), outbox| {
-            state.position = *to;
+        let ship_id = ShipId(ship_id);
 
-            Keep
-        })
-        .with_handler(move |state, ScanPing(at), outbox| {
-            if state.behavior == ShipBehavior::WaitingForPing {
-                state.behavior = ShipBehavior::ApproachingAsteroid(* at);
+        let starting_point = Point3::new(
+            rng.gen_range(-100.0f64 .. 100.0),
+            rng.gen_range(-100.0 .. 100.0),
+            rng.gen_range(-100.0 .. 100.0),
+        );
 
-                let delta = at - state.position;
+        struct ShipMovementController {
+            position: Point3<f64>,
+            destination: Option<Point3<f64>>,
+            ship_id: ShipId,
+        }
 
-                outbox.send(ShipDestination(at - delta.normalize() * 1.0));
+        system.build_actor(ShipMovementController {
+            ship_id,
+            position: starting_point,
+            destination: None
+        }).with_handler(move |state, _:&Tick, outbox| {
+            if let Some(destination) = &state.destination {
+                const SPEED : f64 = 0.1;
+                if (destination - state.position).norm() < SPEED {
+                    state.position = *destination;
+                    state.destination = None;
+
+                    outbox.send(ShipArrived(ship_id));
+                } else {
+                    state.position += (destination - state.position).normalize() * SPEED;
+                }
             }
+            outbox.send(ShipMoved(ship_id, state.position));
 
             Keep
-        })
-        .with_handler(move |state, _:&ShipArrived, outbox| {
-            if let ShipBehavior::ApproachingAsteroid(pos) = &state.behavior {
-                outbox.send(CollectAsteroid(*pos));
-                state.behavior = ShipBehavior::Ready;
-
-                outbox.send(delay::delay_from_now(StartShip, Duration::from_secs(5)));
+        }).with_handler(move |state, ShipDestination(id, pos), outbox| {
+            if *id == state.ship_id {
+                state.destination = Some(*pos);
             }
+                Keep
+        });
 
+        #[derive(PartialEq)]
+        enum ShipBehavior {
+            Ready,
+            WaitingForPing,
+            ApproachingAsteroid(Point3<f64>)
+        }
+
+        struct ShipBehaviorControllerState {
+            position: Point3<f64>, // This shouldn't be in here.
+            behavior: ShipBehavior,
+            ship_id: ShipId
+        }
+
+        system.build_actor(ShipBehaviorControllerState {
+            behavior: ShipBehavior::Ready,
+            position: starting_point,
+            ship_id
+        })
+            .with_handler(move |state, StartShip(id), outbox| {
+                if *id == ship_id {
+                    outbox.send(ScanPulse(state.position));
+                    state.behavior = ShipBehavior::WaitingForPing;
+                }
+
+                Keep
+            })
+            .with_handler(move |state, ShipMoved(id, to), outbox| {
+                if *id == state.ship_id {
+                    state.position = *to;
+                }
+
+                Keep
+            })
+            .with_handler(move |state, ScanPing(at), outbox| {
+                if state.behavior == ShipBehavior::WaitingForPing {
+                    state.behavior = ShipBehavior::ApproachingAsteroid(* at);
+
+                    let delta = at - state.position;
+
+                    outbox.send(ShipDestination(state.ship_id, at - delta.normalize() * 1.0));
+                }
+
+                Keep
+            })
+            .with_handler(move |state, ShipArrived(id), outbox| {
+                if *id == state.ship_id {
+                    if let ShipBehavior::ApproachingAsteroid(pos) = &state.behavior {
+                        outbox.send(CollectAsteroid(*pos));
+                        state.behavior = ShipBehavior::Ready;
+
+                        outbox.send(delay::delay_from_now(StartShip(ship_id), Duration::from_secs(5)));
+                    }
+                }
+
+                Keep
+            });
+
+        let mut miner = window.add_obj(Path::new("models/miner.obj"), Path::new("models"), Vector3::new(1.0,1.0,1.0));
+
+        system.build_actor(miner).with_handler(move |sn,ShipMoved(id, to),_| {
+            if *id == ship_id { sn.set_local_translation(to.coords.cast().into()) };
             Keep
         });
 
-    system.send(delay::delay_from_now(StartShip, Duration::from_secs(5)));
+        system.send(delay::delay_from_now(StartShip(ship_id), Duration::from_secs(5)));
+    }
+
+    system.build_actor(0)
+        .with_handler(move |score, _:&CollectAsteroid, outbox| {*score += 1; outbox.send(Score(*score)); Keep} );
 
     for pt in asteroids.iter().cloned() {
         system.build_actor(())
             .with_handler(move |sn, ScanPulse(pos), outbox| {
                 outbox.send(delay::delay_from_now(ScanPing(pt), Duration::from_secs_f64((pos - pt).norm() / SCAN_PING_SPEED)));
-                eprintln!("Asteroid {:?} echoing: ", pt.coords.data.as_slice());
                 Keep
             })
             .with_handler(move |score, CollectAsteroid(collected_pt), outbox| {
                 if collected_pt == &pt {
-                    eprintln!("Asteroid {:?} can't be pinged.", pt.coords.data.as_slice());
                     End
                 } else {
-                    eprintln!("Asteroid {:?} can still be pinged", pt.coords.data.as_slice());
                     Keep
                 }
             });
@@ -200,14 +232,9 @@ fn main() {
         .with_handler(move |_,c:&CollectAsteroid,_| {println!("Asteroid collected: {:#?}",c); Keep} )
         .with_handler(move |_,Score(score),_| {println!("Score: {}", score); Keep} );
 
-    let mut window = Window::new("Kiss3d: cube");
 
-    let mut miner = window.add_obj(Path::new("models/miner.obj"), Path::new("models"), Vector3::new(1.0,1.0,1.0));
 
-    system.build_actor(miner).with_handler(|sn,ShipMoved(to),_| {
-        sn.set_local_translation(to.coords.cast().into());
-        Keep
-    });
+
 
     let mut ringstation = window.add_obj(Path::new("models/ringstation.obj"), Path::new("models"), Vector3::new(1.0,1.0,1.0));
 
@@ -225,6 +252,7 @@ fn main() {
         system.build_actor(ball).with_handler(move |ball,CollectAsteroid(the_roid),_| {
             if &roid == the_roid {
                 ball.unlink();
+                println!("Roidn't");
                 End
             } else {
                 Keep
